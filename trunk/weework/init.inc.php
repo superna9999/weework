370d3e34902398cd41631ba57d3bef3c5d5e5e6d
superna9999 <superna9999@04bbdda6-231b-0410-8e20-035b669f1ed7>
2006-08-16 18:33:37 +0000

    base_href

--- trunk/weework/init.inc.php
+++ trunk/weework/init.inc.php
@@ -17,12 +17,22 @@ class wee
         if(!isset($_SESSION["global"]))
         {
             session_register("global");
-        }
+	}
 
-	if(!defined($_SERVER["SCRIPT_NAME"]))
-		$GLOBALS["BASE_HREF"] = "index.php";
+	if(!isset($_SERVER["SCRIPT_NAME"]) || empty($_SERVER["SCRIPT_NAME"]))
+	{
+		if(isset($_SERVER["PHP_SELF"]) && !empty($_SERVER["PHP_SELF"]))	
+		{
+			$GLOBALS["BASE_HREF"] = substr_replace($_SERVER["PHP_SELF"], "", strlen($GLOBALS['PATH_INFO']) * -1);
+		}
+		// fastCgi
+		else if(isset($_SERVER["REDIRECT_URL"]) && !empty($_SERVER["REDIRECT_URL"]))
+		{
+			$GLOBALS["BASE_HREF"] = substr_replace($_SERVER["REDIRECT_URL"], "", strlen($GLOBALS['PATH_INFO']) * -1);
+		}
+	}
 	else
-	        $GLOBALS["BASE_HREF"] = $_SERVER["SCRIPT_NAME"];
+		$GLOBALS["BASE_HREF"] = $_SERVER["SCRIPT_NAME"];
 
         $GLOBALS["global"] =& $_SESSION["global"];
 
